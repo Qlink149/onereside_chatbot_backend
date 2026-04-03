966ad588424f8dfcd0a1f1193dcffd4533316fb7
prathampaleriya <prathampaleriya@gmail.com>
2026-04-04 01:40:44 +0530

    updated mpde;

--- onereside_chatbot/processors/product_search_agent.py
+++ onereside_chatbot/processors/product_search_agent.py
@@ -195,7 +195,7 @@ class ProductAgent(Processor):
 
                 while iteration < MAX_SEARCH_ITERATIONS:
                     response = await openai_client.responses.create(
-                        model="gpt-5",
+                        model="gpt-4.1-mini",
                         instructions=product_recommender_prompt,
                         input=current_messages,
                         tools=[search_products_tool, get_product_by_id_tool, compare_products_tool],
