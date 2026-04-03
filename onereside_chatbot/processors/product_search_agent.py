17f3190cfdf2c21fbd423c3dd0acc6353cdd27fb
prathampaleriya <prathampaleriya@gmail.com>
2026-04-04 01:41:35 +0530

    model to 4.1 mini

--- onereside_chatbot/processors/product_search_agent.py
+++ onereside_chatbot/processors/product_search_agent.py
@@ -203,7 +203,7 @@ class ProductAgent(Processor):
                         parallel_tool_calls=False,
                         text=output_schema,
                         max_output_tokens=1200,
-                        reasoning={"effort": "low"} 
+                        # reasoning={"effort": "low"} 
                     )
 
                     logger.info(
